7574c8cae7162bef2ff1db70e5cd655c7aa943fa
Denis <avenueftw@ukr.net>
2022-07-30 00:04:50 +0300

    save hw3

---
+++ scripts/scripts.js
@@ -0,0 +1,453 @@
+// Задание №1 ======
+// var number = 25 + 50;
+// console.log(number);
+//
+// var str = "Говнецо";
+// console.log(str);
+//
+// var qwes = true;
+// console.log(qwes);
+//
+// var trust= undefined;
+// console.log(trust);
+//
+// var muNull = null;
+// console.log(muNull);
+//
+// var symbol = Symbol("id");
+// console.log(symbol);
+//
+// var bigInt = 1000000n;
+// console.log(bigInt);
+//
+// var myObj = {
+//     name: 'Jason',
+//     lastName:'Statham',
+//     age: 54
+// };
+// console.log(myObj);
+//
+// //Задание №2 ======
+// var number = 300;
+//
+// number = 900;
+// console.log(number);
+//
+// let message = 'Hello!';
+// message = 'goodbye';
+// console.log(message);
+//
+// const variable = 5;
+// console.log(variable);
+
+//Задание №3======
+
+////Строка
+
+// let str = "32145";
+// console.log(typeof str);
+// console.log(str);
+//
+// let one = +str;
+//  console.log(typeof one);
+//  console.log(one);
+//
+//  let two = !!str;
+// console.log(typeof two);
+// console.log(two);
+
+////Число
+
+// let number = "55552";
+// console.log(typeof number);
+// console.log(number);
+//
+// let one = +number;
+//  console.log(typeof one);
+//  console.log(one);
+//
+//  let two = !!number;
+// console.log(typeof two);
+// console.log(two);
+
+////Undefined
+
+// let und = undefined;
+// console.log(typeof und);
+// console.log(und);
+//
+// let one = +und;
+//  console.log(typeof one);
+//  console.log(one);
+//
+//  let two = !!und;
+// console.log(typeof two);
+// console.log(two);
+//
+// let three = String(und);
+// console.log(typeof three);
+// console.log(three);
+
+////Null
+
+// let nul = null;
+// console.log(typeof nul);
+// console.log(nul);
+//
+// let one = +null;
+// console.log(typeof one);
+// console.log(one);
+//
+// let two = !!null;
+// console.log(typeof two);
+// console.log(two);
+//
+// let three = String(nul);
+// console.log(typeof three);
+// console.log(three);
+
+////Boolean
+
+// let bool = true;
+// console.log(typeof bool);
+// console.log(bool);
+//
+// let one = +bool;
+// console.log(typeof one);
+// console.log(one);
+//
+// let two = String(bool);
+// console.log(typeof two);
+// console.log(two);
+
+////BigInt
+
+// let big = BigInt("222234526");
+// console.log(typeof big);
+// console.log(big);
+//
+// let one = String(big);
+// console.log(typeof one);
+// console.log(one);
+//
+// let two = !!big;
+// console.log(typeof two);
+// console.log(two);
+
+////Symbol
+
+// let symb = Symbol("552123");
+// console.log(typeof symb);
+// console.log(symb);
+//
+// let one = !!symb;
+// console.log(typeof one);
+// console.log(one);
+//
+// let two = String(symb);
+// console.log(typeof two);
+// console.log(two);
+
+
+// let obj = {s:4};
+// console.log(typeof obj);
+// console.log(obj);
+//
+// let one = !!obj;
+// console.log(typeof one);
+// console.log(one);
+//
+// let two = String(obj);
+// console.log(two);
+// console.log(two);
+
+//Задание №4======
+
+//// Строка
+
+// let str = 'test text';
+// let str1 = 'test text';
+// let strstr = str + str1;
+// console.log(strstr);
+//
+////
+// let str0 = 'text text';
+// let num = '123321';
+// let strnum = str0 + num;
+// console.log(strnum);
+
+////
+
+// let str2 = 'text text';
+// let unt = undefined;
+// let strunt = str2 + unt;
+// console.log (strunt);
+
+////
+
+// let str3 = 'text text';
+// let bool = true;
+// let strbool = str3 + bool;
+// console.log(strbool);
+
+////
+
+// let str4 = 'text text';
+// let bigInt = ('123321');
+// let strbigInt = str4 + bigInt;
+// console.log(strbigInt);
+
+////
+
+// let str5 = 'text text';
+// let nul = null;
+// let strnul = str5 + nul;
+// console.log(strnul);
+
+////
+
+// let str6 = 'text text';
+// let symB = Symbol('123321');
+// let strsymB = str6 + symB;
+// console.log(strsymb);
+
+////
+
+// let str7 = 'text text';
+// let obj = {a:12};
+// let strobj = str7 + obj;
+// console.log(strobj);
+
+//// Число
+
+// let num = 1223;
+// let numbs = 100;
+// let numnumbs = num + numbs;
+// console.log(numnumbs);
+
+////
+
+// let num1 = 123;
+// let strs = 'Keks';
+// let numstrs = num1 + strs;
+// console.log(numstrs);
+
+////
+
+// let num2 = 322;
+// let und1 = undefined;
+// let num2und = num2 + und1;
+// console.log(num2und);
+
+////
+
+// let num3 = 4411;
+// let bools = true;
+// let num3bool = num3 + bools;
+// console.log(num3bool);
+//
+
+////
+
+//
+// let num4 = 1253;
+// let nul2 = null;
+// let num4nul = num4 + nul2;
+// console.log(num4nul);
+
+////
+
+// let num5 = 11111;
+// let bigInt1 = ('123321');
+// let num5big = num5 + bigInt1;
+// console.log(num5big);
+
+////
+
+// let num6 = 4422;
+// let symbs = Symbol('5522');
+// let num6symb = num6 + symbs;
+// console.log(num6symb);
+
+////
+
+// let num7 = 7676;
+// let obj4 = {s : 12};
+// let num7obj = num7 + obj4;
+// console.log(num7obj);
+
+////
+
+//Задание №5======
+
+//// Строка
+
+// let str10 = 'test text';
+// let str20 = 'test text';
+// let str1str = str10 - str20;
+// console.log(str1str);
+//
+////
+// let str11 = 'text text';
+// let num00 = '123321';
+// let str2num = str11 - num00;
+// console.log(str2num);
+
+////
+
+// let str12 = 'text text';
+// let unt5 = undefined;
+// let str3unt = str12 - unt5;
+// console.log (str3unt);
+
+////
+
+// let str13 = 'text text';
+// let bool5 = true;
+// let str1bool = str13 - bool5;
+// console.log(str1bool);
+
+////
+
+// let str14 = 'text text';
+// let big1Int = ('123321');
+// let str1bigInt = str14 - big1Int;
+// console.log(str1bigInt);
+
+////
+
+// let str15 = 'text text';
+// let 2nul = null;
+// let 2strnul = str15 - 2nul;
+// console.log(2strnul);
+
+////
+
+// let str16 = 'text text';
+// let symB = Symbol('123321');
+// let str1symB = str16 - symB;
+// console.log(str1symB);
+
+////
+
+// let str17 = 'text text';
+// let 1obj4 = {a:12};
+// let 1strobj4 = str17 - 1obj4;
+// console.log(1strobj4);
+
+//// Число
+
+// let num11 = 1223;
+// let 1numbs = 100;
+// let num1numbs = num11 - 1numbs;
+// console.log(num1numbs);
+
+////
+
+// let num12 = 123;
+// let 1strs = 'Keks';
+// let 1numstrs = num12 - 1strs;
+// console.log(1numstrs);
+
+////
+
+// let num13 = 322;
+// let 1und1 = undefined;
+// let 1num2und = num13 - 1und1;
+// console.log(1num2und);
+
+////
+
+// let num14 = 4411;
+// let bo1ols = true;
+// let n1um3bool = num14 - bo1ols;
+// console.log(n1um3bool);
+//
+
+////
+
+//
+// let num15 = 1253;
+// let 1nul2 = null;
+// let 1num4nul = num15 - 1nul2;
+// console.log(1num4nul);
+
+////
+
+// let num16 = 11111;
+// let 1bigInt1 = ('123321');
+// let num15big = num16 - 1bigInt1;
+// console.log(num15big);
+
+////
+
+// let num17 = 4422;
+// let sym1bs = Symbol('5522');
+// let num16symb = num17 - sym1bs;
+// console.log(num16symb);
+
+////
+
+// let num18 = 7676;
+// let o1bj4 = {s : 12};
+// let num17obj = num18 - o1bj4;
+// console.log(num17obj);
+
+////
+
+//Задание №6======
+// let login = prompt('wright login');
+// let password = prompt('write password');
+
+// if (login === 'kykold',password === '1234asdQQ')
+//     alert('Добро пожаловать');
+// else(alert('Ошибка входа'));
+
+//Задание №7======
+
+//let year = +prompt('Year of Birthday');
+//let age = 2022 - year;
+//if (age>= 16)
+//{console.log(age, `Добро пожаловать.`)}
+//else {
+//  		console.log(age, `Вход восприщен.`);
+//  	}
+
+// Задание №8======
+
+// let year = prompt('Year of Work');
+// if(year > 0 && year < 5) {
+//     console.log("your allowance is 0%")
+// } else if (year >= 5 && year < 10) {
+//     console.log("your allowance is 5%")
+// } else if (year >= 10 && year < 20) {
+//     console.log("your allowance is 10%")
+// } else if (year >= 20 && year <= 40) {
+//     console.log("your allowance is 20%")
+// } else if (year > 40 ) {
+//     console.log("you should be retired")
+// } else {
+//     console.log("you dont working here")
+// }
+
+//Задание №9
+
+// function dayWeek(day) {
+// 	if (day === 1) {
+// 		console.log(`Monday`);
+// 	} else if (day === 2) {
+// 		console.log(`Tuesday`);
+// 	} else if (day === 3) {
+// 		console.log(`Wednesday`);
+// 	} else if (day === 4) {
+// 		console.log(`Thursday`);
+// 	} else if (day === 5) {
+// 		console.log(`Friday`);
+// 	} else if (day === 6) {
+// 		console.log(`Saturday`);
+// 	} else if (day === 7) {
+// 		console.log(`Sunday`);
+// 	} else {
+// 		console.log(`not have day like this`);
+// 	}
+//  }
